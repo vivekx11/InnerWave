69d35b3caa56407830479ae3cc81d6d9b532a8eb
Vivek Sawji <168280120+vivekx11@users.noreply.github.com>
2026-03-10 21:09:39 +0530

    Update setting.dart

--- lib/setting.dart
+++ lib/setting.dart
@@ -27,7 +27,7 @@ class _SettingsPageState extends State<SettingsPage> {
       }
     }
   }
-
+//follow up code
   @override
   Widget build(BuildContext context) {
     const black = Color(0xFF0E0F14);
